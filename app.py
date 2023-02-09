1ff9c25c1fc9c216368a135e0856b2adcf9e4a96
SwatiSTiwari <swatistiwati13@gmail.com>
2023-02-09 21:15:06 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template,request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+# from sqlalchemy import create_engine
+# engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+
+app = Flask(__name__,template_folder="templates")
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///swati.db"
+app.config["SQLLALCHEMY_TRACK_MODIFICATION"]=False
+db = SQLAlchemy(app)
+
+
+
+class Todo (db.Model):
+    sno=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(200),nullable=False)
+    desc=db.Column(db.String(500),nullable=False)
+    date=db.Column(db.DateTime,default=datetime.utcnow )
+    
+    def __repr__(self)->str:
+        return f"{self.sno}-{self.title}"
+    
+with app.app_context():
+    db.create_all() 
+
+
+
+
+
+@app.route("/",methods=['get','post'])
+def hello_world():
+    if request.method=='post':
+        title=request.form['title']
+        desc=request.form['desc']
+        todo = Todo(title= title,desc=desc)
+        
+    # title=request.form['title']
+    # desc=request.form['desc']
+    # todo = Todo(title= title,desc=desc)
+        
+    
+    db.session.add(todo)
+    db.session.commit()
+    allTodo= Todo.query.all()
+    print(title,desc)
+    return render_template("index.html",allTodo=allTodo)
+
+
+
+@app.route("/show")
+def products():
+    allTodo= Todo.query.all()
+    print(allTodo)
+    return 'this is product page'
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
